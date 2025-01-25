cfca1798452d885119fbe07b2e9d4ec97ccc51b5
Sophie Silver <87021304+SophieSilver@users.noreply.github.com>
2025-01-25 23:29:42 +0300

    refactor: removed the unused app page

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -2,16 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';
 
-import 'package:so_simple_tracker/widgets/app_page.dart';
-import 'package:so_simple_tracker/widgets/home.dart';
-import 'package:so_simple_tracker/widgets/track_page.dart';
+import '/widgets/home.dart';
 
 part '../generated/widgets/app.g.dart';
 
 @swidget
 Widget _app() {
   return MaterialApp(
-    title: "So Simple Tracker",
+    title: "Soracker",
     localizationsDelegates: [
       GlobalMaterialLocalizations.delegate,
       GlobalWidgetsLocalizations.delegate,
